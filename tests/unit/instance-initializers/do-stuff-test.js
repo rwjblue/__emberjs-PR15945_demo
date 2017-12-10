30a36e61691bf4c13ff0321726dc927b9aa93686
Robert Jackson <rjackson@linkedin.com>
2017-12-09 21:13:48 -0500

    Update to public API's for instance-initializers.

--- tests/unit/instance-initializers/do-stuff-test.js
+++ tests/unit/instance-initializers/do-stuff-test.js
@@ -1,25 +1,29 @@
 import Application from '@ember/application';
-import { run } from '@ember/runloop';
 import { initialize } from 'testing-testing-2-17/instance-initializers/do-stuff';
 import { module, test } from 'qunit';
 import destroyApp from '../../helpers/destroy-app';
 
-module('Unit | Instance Initializer | do stuff - old', {
-  beforeEach() {
-    run(() => {
-      this.application = Application.create();
-      this.appInstance = this.application.buildInstance();
+module('Unit | Instance Initializer | do stuff', function(hooks) {
+  hooks.beforeEach(function() {
+    this.TestApplication = Application.extend();
+    this.TestApplication.instanceInitializer({
+      name: 'do stuff',
+      initialize
     });
-  },
-  afterEach() {
-    run(this.appInstance, 'destroy');
+
+    this.application = this.TestApplication.create({ autoboot: false });
+    this.instance = this.application.buildInstance();
+  });
+
+  hooks.afterEach(function() {
+    destroyApp(this.instance);
     destroyApp(this.application);
-  }
-});
+  });
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  initialize(this.appInstance);
+  // Replace this with your real tests.
+  test('it works', async function(assert) {
+    await this.instance.boot();
 
-  assert.equal(self.foo, true);
+    assert.equal(self.foo, true);
+  });
 });
